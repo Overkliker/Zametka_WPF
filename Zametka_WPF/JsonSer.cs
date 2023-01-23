27914ca34d53baadb6e32024c840538e96aa48a7
Данила Хренов <danilahreno@gmail.com>
2023-01-23 15:39:12 +0300

    next1

---
+++ Zametka_WPF/JsonSer.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zametka_WPF
+{
+    internal class JsonSer
+    {
+        public static string cur = Directory.GetCurrentDirectory();
+        public static string file = cur.Substring(0, cur.Length - 24) + "Notes.json";
+
+        public static void Ser(Dictionary<string, List<Note>> obj)
+        {
+            string json = JsonConvert.SerializeObject(obj);
+            File.WriteAllText(file, json);
+        }
+
+        public static Dictionary<string, List<Note>> Des()
+        {
+            Debug.WriteLine(file);
+            string json = File.ReadAllText(file);
+            Dictionary<string, List<Note>> notes = JsonConvert.DeserializeObject<Dictionary<string, List<Note>>>(json);
+
+            return notes;
+        }
+    }
+}
--- Zametka_WPF/MainWindow.xaml.cs
+++ Zametka_WPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
 
 namespace Zametka_WPF
 {
@@ -23,6 +25,58 @@ namespace Zametka_WPF
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void MakeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (DescriptionBox.Text != "" && NameZamBox.Text != "")
+            {
+                var des = JsonSer.Des();
+                if (des.ContainsKey(datePicker1.SelectedDate.ToString()) && des.Keys != null)
+                {
+                    Note newNote = new Note();
+                    newNote.descrption = DescriptionBox.Text;
+                    newNote.name = NameZamBox.Text;
+                    newNote.dateTime = datePicker1.SelectedDate.ToString();
+                    des[datePicker1.SelectedDate.ToString()].Add(newNote);
+                }
+                else
+                {
+                    Note newNote = new Note();
+                    newNote.descrption = DescriptionBox.Text;
+                    newNote.name = NameZamBox.Text;
+                    newNote.dateTime = datePicker1.SelectedDate.ToString();
+
+                    List<Note> newDate = new List<Note>();
+                    newDate.Add(newNote);
+                    des.Add(newNote.dateTime, newDate);
+                }
+
+                JsonSer.Ser(des);
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Zametki_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int ind = Zametki.SelectedIndex;
+            string date = datePicker1.SelectedDate.ToString();
+            Note des = JsonSer.Des()[date][ind];
+
+            DescriptionBox.Text = des.descrption;
+            NameZamBox.Text = des.name;
+
+
         }
     }
 }
